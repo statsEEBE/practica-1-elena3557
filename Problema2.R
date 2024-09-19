97b43390d775d3fb467ecb372f29767a9b577d74
elena.perez.salazar <elena.perez.salazar@auleseebe.upc.edu>
2024-09-19 11:42:19 +0200

    ...

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,9 @@
 #Codigo para problema 2
+
+ls() #me lista todas las variables que he definido en el programa hasta el momento
+rm(list=ls()) #me elimina todas las variables hasta el momento
+
+data <- mtcars
+data
+
+hist(data$mpg) #respuesta
